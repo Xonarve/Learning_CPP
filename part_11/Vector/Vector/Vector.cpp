c075eea66d4b6732c382b62d39748d75b9494e23
Peter <p.silkin@inbox.ru>
2021-12-06 12:25:58 +0300

    adding some code for vector

--- part_11/Vector/Vector/Vector.cpp
+++ part_11/Vector/Vector/Vector.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 #include <iostream>
 #include <cmath>
+#include <cstdlib>
+#include <ctime>
 
 class Vector {
 public:
@@ -41,6 +43,38 @@ public:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	using namespace std;
+
+	/*Vector v1(12.34, 7.42, Vector::POL);
+	cout << v1 << endl;
+
+	Vector v2(66.21, 97.15, Vector::POL);
+	cout << v2 << endl;
+
+	Vector v3(0.0, 0.0, Vector::POL);
+	v3 = v1 + v2;
+	cout << v3 << endl;*/ //INCORRECT! only for mode = RECT for both
+
+	srand(time(0));
+	double direction;
+	Vector step;
+	Vector result;
+	unsigned long steps = 0;
+	double target;
+	double dstep;
+
+	cout << "Enter target distance: (q to quit) ";
+	while (cin >> target) {
+		cout << "Enter a step length: ";
+		if (!(cin >> dstep))
+			break;
+		while (result.magval < target) {
+			direction = rand() % 360;
+			step.reset(dstep, direction, Vector::POL);
+			result = 
+		}
+	}
+
 	return 0;
 }
 
@@ -112,3 +146,33 @@ void Vector::reset(double n1, double n2, Mode form) {
 	}
 }
 
+Vector Vector::operator+(const Vector& v) const {
+	return Vector(x + v.x, y + v.y);
+}
+
+Vector Vector::operator-(const Vector& v) const {
+	return Vector(x - v.x, y - v.y);
+}
+
+Vector Vector::operator-() const {
+	return Vector(-x, -y);
+}
+
+Vector Vector::operator*(double m) const {
+	return Vector(x * m, y * m);
+}
+
+Vector operator*(double m, const Vector& v) {
+	return v * m;
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector& v) {
+	using namespace std;
+	if (v.mode == Vector::RECT)
+		os << "(x, y) = (" << v.x << ", " << v.y << ")";
+	else if (v.mode == Vector::POL)
+		os << "(m, a) = (" << v.mag << ", " << v.ang * Rad_to_deg << ")";
+	else
+		os << "Vector mode's invalid/a";
+	return os;
+}
