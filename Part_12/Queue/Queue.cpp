cb4dd4f8cb5210dc941947c60b5d51b29a30ecd0
Peter <p.silkin@inbox.ru>
2022-01-27 01:19:29 +0300

    learning part_12

---
+++ Part_12/Queue/Queue.cpp
@@ -0,0 +1,36 @@
+// Queue.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+typedef int Item;
+
+class Queue {
+	enum {Q_SIZE = 10};
+	struct Node {
+		Item item;
+		struct Node *next;
+	};
+	Node *front;
+	Node *rear;
+	int items;
+	const int qsize;
+public:
+	Queue(int qs = Q_SIZE);
+	~Queue();
+	bool isempty() const;
+	bool isfull() const;
+	int queueconst() const;
+	bool enqueue(const Item& item);
+	bool dequeue(Item& item);
+};
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+
+Queue::Queue(int qs) : qsize(qs) {
+	front = rear = nullptr;
+	items = 0;
+}
