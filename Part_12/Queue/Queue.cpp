4c22eab97c1c9e2192a5219c7856cf98ea0dbc22
Peter Silkin <p.silkin@inbox.ru>
2022-01-31 23:33:51 +0300

    learning part_12

--- Part_12/Queue/Queue.cpp
+++ Part_12/Queue/Queue.cpp
@@ -2,8 +2,24 @@
 //
 
 #include "stdafx.h"
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 
-typedef int Item;
+const int MIN_PER_HR = 60;
+bool newcustomer(double x);
+
+class Customer {
+	long arrive;
+	long processtime;
+public:
+	Customer() { arrive = processtime = 0; }
+	void set(long when);
+	long when() const { return arrive; }
+	long ptime() const { return processtime; }
+};
+
+typedef Customer Item;
 
 class Queue {
 	enum { Q_SIZE = 10 };
@@ -15,6 +31,9 @@ class Queue {
 	Node *rear;
 	int items;
 	const int qsize;
+
+	Queue(const Queue &q) : qsize(0) {  };
+	Queue &operator=(const Queue &q) { return *this; };
 public:
 	Queue(int qs = Q_SIZE);
 	~Queue();
@@ -27,6 +46,74 @@ public:
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	using std::cout;
+	using std::cin;
+	using std::endl;
+	using std::ios_base;
+
+	std::srand(std::time(0));
+	cout << "Case study: Bank of Heahter Automatic Teller\n";
+	cout << "Enter maximum size of queue: ";
+	int qs;
+	cin >> qs;
+
+	Queue line(qs);
+
+	cout << "Enter the number of simulation hours: ";
+	int hours;
+	cin >> hours;
+	
+	long cyclelimit = MIN_PER_HR * hours;
+	cout << "Enter the number of customers per hour: ";
+	double perhour;
+	cin >> perhour;
+
+	double min_per_cust;
+	min_per_cust = MIN_PER_HR / perhour;
+
+	Item temp;
+	long turnaways = 0;
+	long customers = 0;
+	long served = 0;
+	long sum_line = 0;
+	long wait_time = 0;
+	long line_wait = 0;
+
+	for (int cycle = 0; cycle < cyclelimit; ++cycle) {
+		if (newcustomer(min_per_cust)) {
+			if (line.isfull())
+				++turnaways;
+			else {
+				++customers;
+				temp.set(cycle);
+				line.enqueue(temp);
+			}
+		}
+		if (wait_time <= 0 && !line.isempty()) {
+			line.dequeue(temp);
+			wait_time = temp.ptime();
+			line_wait += cycle - temp.when();
+			++served;
+		}
+		if (wait_time > 0)
+			--wait_time;
+		sum_line += line.queueconst();
+	}
+	if (customers > 0) {
+		cout << "customers accepted: " << customers << endl;
+		cout << "customers served: " << served << endl;
+		cout << "turnaways: " << turnaways << endl;
+		cout << "average queue size: ";
+		cout.precision(2);
+		cout.setf(ios_base::fixed, ios_base::floatfield);
+		cout << (double)sum_line / cyclelimit << endl;
+		cout << "average wait time: "
+			<< (double)line_wait / served << " minutes\n";
+	}
+	else
+		cout << "No customers!\n";
+	
+	cout << "Done!\n";
 	return 0;
 }
 
@@ -82,5 +169,12 @@ Queue::~Queue() {
 		front = front->next;
 		delete temp;
 	}
+}
 
+void Customer::set(long when) {
+	processtime = std::rand() % 3 + 1;
+}
+
+bool newcustomer(double x) {
+	return std::rand() * x / RAND_MAX < 1;
 }
